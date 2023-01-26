6e6212831720ff133d8cf81d3a68b69412c674b9
Sedad1337 <77668127+Sedad1337@users.noreply.github.com>
2023-01-26 20:47:49 +0100

    Update DefaultController.php

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,7 +11,7 @@ class DefaultController extends AbstractController
     #[Route('/')]
     public function index(): Response
     {
-        return $this->render('sedad.html', [
+        return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
     }
